aa29baf27bb63a564d1200461ceee2ec251af463
mrówa <piotr.nalewajka@gmail.com>
2016-03-15 21:52:41 +0100

    Add basic project for PyCharm and www link scraper

---
+++ scraper.py
@@ -0,0 +1,65 @@
+import requests
+from lxml import html
+import sys
+import random
+
+linksToCheck = set([])
+linksVisited = []
+linksWithErrors = []
+
+def getLinksFromUrl (url):
+    linksToCheck.remove(url)
+    links = []
+    try:
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+        links = tree.xpath('//a/@href')
+        linksVisited.append(url)
+    except:
+        # print('ooops, can''t do it for {' + url + '}, error', sys.exc_info()[0])
+        linksWithErrors.append(url)
+        print('url error', url)
+        return []
+
+    domain = url.replace('https:', '')
+    domain = domain.replace('http:', '')
+    domain = domain.replace('//', '')
+    if domain.startswith('/'):
+        domain = domain[1:]
+    domain = domain.split('/')[0]
+
+    correctedLinks = []
+    for link in links:
+        if not link.startswith('http'):
+            if link.startswith('//'):
+                link = 'http:' + link
+                print(1, link, url)
+            elif link.startswith('/'):
+                base = url.split('/')[1]
+                link = 'http://'+ domain + '/' + link
+                print(2, link, base, url)
+            else:
+                base = url.split('/')[0]
+                link = 'http://' + domain + '/' + link
+                print(3, link, base, url)
+
+        correctedLinks.append(link)
+
+    for newLink in correctedLinks:
+        linksToCheck.add(newLink)
+
+#    if not 'stackoverflow.com' in url:
+#        print('finished, found', len(links), 'links on', url)
+    print('[' + url[0:40] +'] to do', len(linksToCheck),'good', len(linksVisited), 'bad', len(linksWithErrors))
+
+    return correctedLinks
+
+firstLink = 'http://stackoverflow.com'
+# found = getLinksFromUrl(firstLink)
+
+linksToCheck.add(firstLink)
+
+
+while len(linksToCheck) > 0:
+    link = random.sample(linksToCheck,1)
+    getLinksFromUrl(link[0])
